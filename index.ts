aecd961af9ece5fe2fefd066483c8e7710b7d684
Artur <arab27696797@gmail.com>
2026-03-23 14:39:02 +0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+/**
+ * Модули RentierGuard
+ * Экспорт всех модулей приложения
+ */
+
+export * as user from './user';
+export * as property from './property';
+export * as tax from './tax';
+export * as contract from './contract';
+export * as payment from './payment';
+export * as document from './document';
+export * as expert from './expert';
+export * as problem from './problem';
