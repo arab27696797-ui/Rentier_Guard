cd9f1e7e6789e9c7ce97d8032481f6c43544aaea
Artur <arab27696797@gmail.com>
2026-03-24 15:54:54 +0200

    Update prisma.service.ts

--- src/core/services/prisma.service.ts
+++ src/core/services/prisma.service.ts
@@ -9,11 +9,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: process.env.NODE_ENV === 'development'
-    ? ['query', 'error', 'info', 'warn']
-    : ['error'],
-});
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'info', 'warn']
+        : ['error'],
+  });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
